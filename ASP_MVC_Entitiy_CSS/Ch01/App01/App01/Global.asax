cc312675d94dfbfbf54475b493891a6867946ff7
szykov <szykov@dataart.com>
2018-12-19 21:33:10 +0300

    Chapter 1

---
+++ ASP_MVC_Entitiy_CSS/Ch01/App01/App01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App01.MvcApplication" Language="C#" %>
---
+++ ASP_MVC_Entitiy_CSS/Ch01/App01/App01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(App01.Startup))]
+namespace App01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
